01eac3eb818a725a25d02094fb16c475dd619d26
JasonBason <jsh339@cornell.edu>
2024-02-18 11:44:42 -0500

    commit 3

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,13 +1,18 @@
-#' Title
+#' Split a string
 #'
 #' @param string A character vector
 #' @param pattern A regex pattern
 #' @param n An integer
+#' @inheritParams stringr::str_split
 #'
-#' @return
+#' @return A character vector
 #' @export
 #'
 #' @examples
+#' Made by Jason
+#'
+#' y <- "192.168.0.1"
+#' str_split_one(y, pattern = stringr::fixed("."))
 str_split_one <- function(string, pattern, n = Inf){
   stopifnot(is.character(string), length(string)<=1)
   if (length(string)==1){
