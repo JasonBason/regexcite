d76e1b1085f1738530db71a2ba901f70a10b01fd
JasonBason <jsh339@cornell.edu>
2024-02-18 11:34:34 -0500

    commit 2?

---
+++ R/str_split_one.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param string A character vector
+#' @param pattern A regex pattern
+#' @param n An integer
+#'
+#' @return
+#' @export
+#'
+#' @examples
+str_split_one <- function(string, pattern, n = Inf){
+  stopifnot(is.character(string), length(string)<=1)
+  if (length(string)==1){
+    stringr::str_split(string = string,
+                       pattern = pattern,
+                       n = n)[[1]]
+  }else{
+    character()
+  }
+}
